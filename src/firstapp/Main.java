5eed0a4db28667220a984b98d1b6bfd841ae844b
alxc <alex.ciobanu92@gmail.com>
2020-07-07 23:22:20 +0300

    First comiit

---
+++ src/firstapp/Main.java
@@ -0,0 +1,11 @@
+package firstapp;
+
+import firstapp.ui.UserManagementUI;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+       new UserManagementUI().startProgram();
+    }
+}
